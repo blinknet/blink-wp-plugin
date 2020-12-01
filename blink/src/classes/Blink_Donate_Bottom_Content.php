b52145163565e84e3cde21bff8ae90a0b86e25f2
stmihai1337 <stancu.mihai1337@gmail.com>
2020-12-01 16:20:28 +0200

    Fix double render

--- blink/src/classes/Blink_Donate_Bottom_Content.php
+++ blink/src/classes/Blink_Donate_Bottom_Content.php
@@ -8,17 +8,16 @@ class Blink_Donate_Bottom_Content
 {
     static function add_donation_after_content($content)
     {
-        if(!is_single()){
-            return $content;
-        }
-        $show_donate_after_article = get_option(Constants::DATABASE_OPTIONS_DONATE_AFTER_CONTENT);
-        if ($show_donate_after_article == Constants::DONATIONS_AFTER_EACH_ARTICLE) {
-            ob_start();
-            ?>
-            <div id='blink-donation-container'></div>
-            <?php
-            $blink_donate = ob_get_clean();
-            return $content . $blink_donate;
+        if(is_singular() && in_the_loop() && is_main_query() ){
+            $show_donate_after_article = get_option(Constants::DATABASE_OPTIONS_DONATE_AFTER_CONTENT);
+            if ($show_donate_after_article == Constants::DONATIONS_AFTER_EACH_ARTICLE) {
+                ob_start();
+                ?>
+                <div id='blink-donation-container'></div>
+                <?php
+                $blink_donate = ob_get_clean();
+                return $content . $blink_donate;
+            }
         }
         return $content;
     }
